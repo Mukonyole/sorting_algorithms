ca69e675c99af89be30aa226ff2a84924389c822
Mukonyole Evans <emukonyole@gmail.com>
2024-04-26 02:35:00 -0700

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,27 +2,27 @@
 #include <stdio.h>
 
 /**
- * bubble_sort - sorting of array lements from min to max value.
- * @array: an array.
- * @size: an array size.
+ * bubble_sort - sorts an array of integers in
+ * ascending order using a bubble sort algorithm.
+ * @array: array
+ * @size: array size
  */
-
 void bubble_sort(int *array, size_t size)
 {
 
 	size_t i, index, tmp = 0;
 
 	if (size < 2)
-	return;
+		return;
 	for (i = 0; i < size; i++)
 		for (index = 0; index < size; index++)
 		{
 			if (array[index] > array[index + 1] && array[index + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				tmp = array[index];
+				array[index] = array[index + 1];
+				array[index + 1] = tmp;
+				print_array(array, size);
 			}
 		}
 }
