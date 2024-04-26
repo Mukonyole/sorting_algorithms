11b5b1880e4acd97518388841636f99767cb85c9
Mukonyole Evans <emukonyole@gmail.com>
2024-04-26 02:40:10 -0700

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,22 +7,23 @@
  * @array: array
  * @size: array size
  */
+
 void bubble_sort(int *array, size_t size)
 {
-
-	size_t i, index, tmp = 0;
+	size_t i, index;
+	int tmp;
 
 	if (size < 2)
 		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
-		{
-			if (array[index] > array[index + 1] && array[index + 1])
-			{
+
+	for (i = 0; i < size - 1; i++) {
+		for (index = 0; index < size - i - 1; index++) {
+			if (array[index] > array[index + 1]) {
 				tmp = array[index];
 				array[index] = array[index + 1];
 				array[index + 1] = tmp;
 				print_array(array, size);
 			}
 		}
+	}
 }
